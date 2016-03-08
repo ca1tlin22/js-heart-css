936f79364a5e4c3183979d63833e27332c146272
ca1tlin22 <gagn0372@algonquinlive.com>
2016-03-08 16:40:33 -0500

    Almost everything.  Oops. List items from 1-12 added, still working on items 6-9

---
+++ js/main.js
@@ -0,0 +1,33 @@
+var $header = $('header')
+$header.addClass ('masthead');
+
+var $h1 = $('h1');
+$('h1').removeClass('big-heading')
+$('h1').addClass('logo');
+
+var $main = $('main')
+$('main').addClass('wrapper');
+
+var $main = $('main p:first-child')
+$('main p:first-child').addClass('intro');
+
+var $h2 = $('h2')
+$('h2').addClass('slug-head');
+
+var $main = $('main li')
+$('main li').addClass('slug-list');
+
+var $img = $('img')
+$('img').addClass('slug-img');
+
+var $main = $('main li:first-child')
+$('li:first-child').addClass('slug-list-first');
+
+var $main = $('main li:last-child')
+$('li:last-child').addClass('slug-list-last');
+
+var $dl = $('dl')
+$('dl').addClass('classification');
+
+var $dt = $('dt');
+$('dt').removeClass('big-label');
